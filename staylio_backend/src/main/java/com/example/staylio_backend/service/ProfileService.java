ff670be4d1d5c1b350762b7dd3805a5249d3df67
hoang261105 <huyhoangcs2611@gmail.com>
2026-04-29 14:09:42 +0700

    Feat: Profile Update

---
+++ staylio_backend/src/main/java/com/example/staylio_backend/service/ProfileService.java
@@ -0,0 +1,12 @@
+package com.example.staylio_backend.service;
+
+import com.example.staylio_backend.dto.request.ProfileRequest;
+import com.example.staylio_backend.dto.response.ProfileResponseDTO;
+
+import java.io.IOException;
+
+public interface ProfileService {
+    ProfileResponseDTO getUserProfileById(long id);
+
+    ProfileResponseDTO updateInfo(Long id, ProfileRequest profileRequest) throws IOException;
+}
